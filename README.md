606acc27106a1edbd97d2e240ef6b6464d6d5e4d
George Williams <george.williams@gmail.com>
2021-06-02 22:51:11 +0000

    first working test script using the stress utility

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ipmi
+
+## Prerequisites
+
+This was developed and tested with the following:
+* Ubuntu 18.04.
+* Anaconda Python ( using a python=37 environment )
+* Installed "ipmitool" via apt-get, version 1.8.18
+
--- tests/test_with_stress.sh
+++ tests/test_with_stress.sh
@@ -1,8 +1,41 @@
 #!/bin/bash
 
-SERVER="http://192.168.99.35:3000"
+SERVER="http://192.168.99.112:3000"
+SLEEP=10
+STRESSTIME=30
+STRESS_CPU_RANGE=56
 
-# Make sure to start the ipmiserve on a remote machine
+log_it () {
+	curl --silent "$SERVER/log?$1" > /dev/null
+	if [ ! "$?" -eq "0" ]; then
+		echo "Message send failed."
+		exit 1
+	fi
+}
 
-curl -i $SERVER
-echo "$?"
+echo "Testing connection to ipmiserve"
+log_it "test=test_with_stress"
+
+START=1
+END=$STRESS_CPU_RANGE
+for (( CPU=$START; CPU<=$END; CPU++ ))
+do
+	echo
+	echo "Starting a stress test with cpu=$CPU"
+
+	echo "Start a timed sleep = $SLEEP"
+	log_it "sleep=$SLEEP"
+	sleep $SLEEP
+
+	echo "Launch stress cpu=$CPU"
+	log_it "stress=1&time=$STRESSTIME&cpu=$CPU"
+	stress --timeout $STRESSTIME --cpu $CPU
+
+	echo "Stop stress"
+	log_it "stress=0"
+
+done
+	
+echo "Done."
+log_it "done=1"
+exit 0
