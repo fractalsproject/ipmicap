17eab9068c7e807956b30911a2fe68271184277e
George Williams <george.williams@gmail.com>
2021-06-02 20:49:10 +0000

    ready for first test

--- ipmimon.py
+++ ipmimon.py
@@ -20,10 +20,27 @@ class IpmiMon:
 		self.max_consec_errors = max_consec_errors
 		self.delay 		= delay
 		self.logger		= logger
+		self.connected	= False
+		self.consec_errors = 0
+		self.interface	= None
+		self.connection = None
+		self.device_id	= None
+		self.reservation_id = None
+		self.sensors	= []
 	
 	def run(self):
-		
-		self._get_sensors()
+	
+		if not self.connected:
+			raise Exception("ERR: Not connected to the IPMI interface.")
+	
+		if len(self.sensors)==0:
+			self.get_sensors()
+
+		if self.logger:
+			descriptions = self.get_sensor_descriptions()
+			for descr in descriptions:
+				message = "SENSOR: %s %d %d" % (descr['name'], descr['record_id'], descr['number'] )
+				self.logger.log(message)
 
 		self.consec_errors = 0
 	
@@ -34,11 +51,8 @@ class IpmiMon:
 			time.sleep( self.delay )	
 
 
-	def _get_sensors(self):
+	def connect(self):
 	
-		if len(self.records)==0:
-			raise Exception("ERR: No device/sensor records requests.")
-		
 		self.interface = pyipmi.interfaces.create_interface('ipmitool', interface_type='lan')
 		self.connection = pyipmi.create_connection(self.interface)
 		self.connection.target = pyipmi.Target(ipmb_address=0x20)
@@ -51,11 +65,47 @@ class IpmiMon:
 		if not self.device_id.supports_function('sdr_repository'):
 			raise Exception("ERR: IPMI does not support 'sdr_repository' function.")
 
-		reservation_id = self.connection.reserve_sdr_repository()
+		self.reservation_id = self.connection.reserve_sdr_repository()
+		self.connected = True
+
+	def enumerate_sensors(self):
+
+		if not self.connected:
+			raise Exception("ERR: Not connected to the IPMI interface.")
+
+		if self.device_id.supports_function('sdr_repository'):
+			iter_fct = self.connection.sdr_repository_entries
+		elif device_id.supports_function('sensor'):
+			iter_fct = self.connection.device_sdr_entries
+
+		print("Enumerating all sensors...")
+		for s in iter_fct():
+			try:
+				device_id_string = s.device_id_string.decode("utf-8") 
+				print( "%s\trecord_id=%d" % (device_id_string, s.id) )
+			except:
+				pass
+		print("Done.")
+
+	def get_sensor_descriptions(self):
+
+		if len(self.sensors)==0:
+			self.get_sensors()
+
+		descriptions = []
+		for s in self.sensors:
+			descriptions.append( {'name':s.device_id_string, 'record_id':s.id, 'number':s.number } )
+	
+		return descriptions
+	
+	def get_sensors(self):
+		
+		if len(self.records)==0:
+			raise Exception("ERR: No device/sensor records requests.")
 
 		self.sensors=[]
 		for record_id in self.records:
-			s = self.connection.get_repository_sdr(record_id, reservation_id)
+			s = self.connection.get_repository_sdr(record_id, self.reservation_id)
 			self.sensors.append(s)
 
 		return True
@@ -76,7 +126,7 @@ class IpmiMon:
 			number = None
 			value = None
 			states = None
-
+	
 			if s.type is pyipmi.sdr.SDR_TYPE_FULL_SENSOR_RECORD:
 				(value, states) = self.connection.get_sensor_reading(s.number)
 				number = s.number
@@ -95,6 +145,10 @@ class IpmiMon:
 				print('0x{:04x} | {:3d} | {:18s} | ERR: CC=0x{:02x}'.format( s.id, s.number, s.device_id_string, e.cc))
 			return False
 
+		except:
+			print("Sample Sensor Error:", sys.exc_info()[0] )
+			return False			
+
 		finally:
 			pass
 
@@ -108,9 +162,12 @@ class IpmiMon:
 		else:
 			states = 'na'
 
-		message = "0x%04x | %3s | %-18s | %9s | %s" % (record_id, number, id_string, value, states)
-		if self.logger: self.logger.log(message)
-		else: print(message)
+		if self.logger: 
+			message = "%d : %s" % ( record_id, value)
+			self.logger.log(message)
+		else: 
+			message = "0x%04x | %3s | %-18s | %9s | %s" % (record_id, number, id_string, value, states)
+			print(message)
 
 
 if __name__ == "__main__":
@@ -119,6 +176,6 @@ if __name__ == "__main__":
 						username="admin",
 						password="admin",
 						records=[18,20] )
-
+	ipmimon.connect()
 	ipmimon.run()
 
