a579d04d10b0ecc4430798bbc4a60c5249b8f684
George Williams <george.williams@gmail.com>
2021-06-06 01:09:15 +0000

    gpu stress test

--- tests/test_with_stress.sh
+++ tests/test_with_stress.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 SERVER="http://192.168.99.112:3000"
-SLEEP=20
-STRESSTIME=60
+SLEEP=60
+STRESSTIME=120
+INC=4
 STRESS_CPU_RANGE=56
 VM=1
 IO=1
 HDD=1
+GPU=1
 
 log_it () {
 	curl --silent "$SERVER/log?$1" > /dev/null
@@ -22,11 +24,11 @@ send_uptime () {
 }
 
 echo "Testing connection to ipmiserve"
-log_it "test=test_with_stress&cpurange=${STRESS_CPU_RANGE}&vm=${VM}&io=${IO}&hdd=${HDD}"
+log_it "test=test_with_stress&cpurange=${STRESS_CPU_RANGE}&vm=${VM}&io=${IO}&hdd=${HDD}&gpu=${GPU}"
 
 START=0
 END=$STRESS_CPU_RANGE
-for (( CPU=$START; CPU<=$END; CPU++ ))
+for (( CPU=$START; CPU<=$END; CPU+=$INC ))
 do
 	echo
 	echo "Starting a stress test with cpu=$CPU"
@@ -57,6 +59,35 @@ do
 	log_it "stress=0"
 
 done
+	
+if [ "$GPU" -eq "1" ]; then
+	echo "Start a timed sleep = $SLEEP"
+	log_it "sleep=$SLEEP"
+	sleep $SLEEP
+	
+	echo "Launch stress cpu=gpu"
+	log_it "stress=1&time=$STRESSTIME&cpu=gpu"
+	send_uptime "before_stress"
+		
+	if [ "$VM" -eq "1" ] && [ "$IO" -eq "1" ] && [ "$HDD" -eq "1" ]; then
+		stress --timeout $STRESSTIME --cpu $CPU --vm $CPU --io $CPU --hdd $CPU &
+		./run_gpu_burn.sh $STRESSTIME &
+	elif [ "$VM" -eq "1" ] && [ "$IO" -eq "1" ]; then
+		stress --timeout $STRESSTIME --cpu $CPU --vm $CPU --io $CPU &
+		./run_gpu_burn.sh $STRESSTIME &
+	elif [ "$VM" -eq "1" ]; then
+		stress --timeout $STRESSTIME --cpu $CPU --vm $CPU &
+		./run_gpu_burn.sh $STRESSTIME &
+	else
+		stress --timeout $STRESSTIME --cpu $CPU &
+		./run_gpu_burn.sh $STRESSTIME &
+	fi
+		
+	wait
+
+	echo "Stop stress"
+	log_it "stress=0"
+fi
 	
 echo "Done."
 log_it "done=1"
